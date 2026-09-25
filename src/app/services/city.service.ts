a52560d0e0e76347c77b97e70279163d9ec0c96c
mahitha-123 <basivireddymahitha@gmail.com>
2026-09-25 12:29:43 -0400

    changed text from Delhi to New Delhi

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 
 export const CITIES = [
-  'Bengaluru', 'Mumbai', 'Delhi', 'Chennai', 'Hyderabad',
+  'Bengaluru', 'Mumbai', 'New Delhi', 'Chennai', 'Hyderabad',
   'Pune', 'Kolkata', 'Ahmedabad', 'Jaipur', 'Kochin'
 ];
 
