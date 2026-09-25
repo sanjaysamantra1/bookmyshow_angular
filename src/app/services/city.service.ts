05cf74cba80832fae1ee6417379783c1e4ea1387
Sarika Gurram <gurramsarika2002@gmail.com>
2026-09-25 12:32:06 -0400

    changed the text from jaipur to jaipur_changed

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -2,7 +2,7 @@ import { Injectable, signal } from '@angular/core';
 
 export const CITIES = [
   'Banglore', 'Mumbai', 'Delhi', 'Chennai', 'Hyderabad',
-  'Pune', 'Kolkata', 'Ahmedabad', 'Jaipur', 'Kochi'
+  'Pune', 'Kolkata', 'Ahmedabad', 'Jaipur_changed', 'Kochi'
 ];
 
 @Injectable({ providedIn: 'root' })
